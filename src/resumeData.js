94a8f3f78e27d2de3882d9945de267393061b010
Azim ansari <azimpanjwar@gmail.com>
2020-08-31 15:05:26 +0530

    portfolio using react

---
+++ src/resumeData.js
@@ -0,0 +1,109 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Azim Ansari",
+    "role": "Full stack web developer and blogger",
+    "linkedinId":"",//linkedin id
+    "skypeid": "",// skypeid
+    "roleDescription": "",//description
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"B.tech from MIT Muzaffarpur||Student at attainU||",//put somithing
+    "address":"India",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"MIT muzaffarpur",
+        "specialization":"Electrical Engineering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+        "Achievements":"B.tech degree"
+      },
+      {
+        "UniversityName":"Rajendra College Chapra",
+        "specialization":"Intermediate of arts",
+        "MonthOfPassing":"may",
+        "YearOfPassing":"2014",
+        "Achievements":"!2th degree"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"NA",
+        "specialization":"NA",
+        "MonthOfLeaving":"NA",
+        "YearOfLeaving":"2018",
+        "Achievements":"NA"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"NA",
+        "MonthOfLeaving":"NA",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"python"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/retrocam.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"One of the best benefits of case study testimonials is that it provides a potential lead with a complete customer story. This is great for B2B companies who are trying to convince an entire business to buy their product or service.",
+        "name":"Azim Ansari"
+      },
+      {
+        "description":"One of the best benefits of case study testimonials is that it provides a potential lead with a complete customer story. This is great for B2B companies who are trying to convince an entire business to buy their product or service.",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
